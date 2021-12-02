1d09720ff9786129043c165968f7768f1c52fb39
mboufal <mboufal@hawk.iit.edu>
2021-12-02 15:40:08 -0600

    update all
    adding second nav

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 function firstFunction() {
     alert("Your application was submitted");
 }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
