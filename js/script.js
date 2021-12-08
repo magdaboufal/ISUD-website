4b03dad6f5875ce4baae023029dd7cc237282fad
mboufal <mboufal@hawk.iit.edu>
2021-12-07 23:56:15 -0600

    forking ISUD

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 function firstFunction() {
     alert("Your application was submitted");
 }
+function secondFunction() {
+    alert("Your message was correctly sent!");
+}
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
